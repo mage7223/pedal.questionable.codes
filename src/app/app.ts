342b89e1681cb9fbc9d575c20f8255aaa5b57db2
Kevin Rogers <Kevin@KevinRogers.org>
2025-12-01 19:33:00 -0500

    correction path and project name misspelling

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('pedal-quationable-codes');
+  protected readonly title = signal('pedal-questionable-codes');
 }
